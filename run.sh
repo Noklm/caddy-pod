bac73e4c9df33597256cfaa86171f0afb98aff8b
julien.deslandes <julien.deslandes@protonmail.com>
2025-04-13 14:43:40 +0000

    update caddy to v2.9.1

--- run.sh
+++ run.sh
@@ -2,10 +2,13 @@
 
 podman run -d \
     --name caddy \
-    --publish 8080:80 \
+    --publish 80:80 \
+    --publish 443:443 \
     --restart always \
+    --network slirp4netns \
     --mount type=bind,source=$PWD/Caddyfile,target=/etc/caddy/Caddyfile,readonly,relabel=private \
     --mount type=volume,source=caddy_data,target=/data,relabel=private \
-    caddy:2.8.4-alpine
+    --mount type=bind,source=/media/nextcloud,target=/var/www/html,relabel=shared \
+    caddy:2.9.1-alpine
 
 podman logs -f caddy
